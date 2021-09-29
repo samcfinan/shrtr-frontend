d340804e08e3ad63df5f7ac30abb9ded3c047e45
Sam C. Finan <samcfinan@gmail.com>
2021-09-28 20:57:35 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import type { Component } from "solid-js";
+
+import "tailwindcss/tailwind.css";
+import styles from "./App.module.css";
+import Input from "./components/Input";
+
+const App: Component = () => {
+  return (
+    <div>
+      <div class="flex flex-col justify-center"></div>
+      <Input />
+    </div>
+  );
+};
+
+export default App;
